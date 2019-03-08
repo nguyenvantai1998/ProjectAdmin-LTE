54d06b3a23bcf5362f45ee36e0c6366dcadc6782
Đỗ Khoa <momentofm3@gmail.com>
2019-03-08 10:14:03 +0700

    Service ADD,PUT,DELETE

--- AdminLTE/src/app/services/products/product.service.ts
+++ AdminLTE/src/app/services/products/product.service.ts
@@ -4,8 +4,9 @@ import { Observable } from 'rxjs';
 import { Products } from 'src/app/modules/product.model';
 import { environment } from '@environments/environment.prod';
 
-const url = `${environment.apiPV}/api/v1/products/list?`;
-
+const urlgetAll = `${environment.apiPV}/api/v1/products/list?`;
+const urlAdd = `${environment.apiPV}/api/v1/products/create?`;
+const urlEdit = `${environment.apiPV}/api/v1/products/update/`;
 @Injectable({
   providedIn: 'root'
 })
@@ -26,8 +27,19 @@ export class ProductService {
     return header.set('Content-Type', 'application/json').set('Authorization', `Bearer ${token}`);
   }
 
-  getAllProduct(): Observable<Products> {
-    return this.httpClient.get<Products>(url);
+  getAllProduct(): Observable<Products[]> {
+    return <Observable<Products[]>> this.httpClient.get(urlgetAll);
   }
-
+  getIdProduct(_id:number):Observable<Products>{
+    return <Observable<Products>> this.httpClient.get(`${urlEdit}/${_id}`, {headers: this.headers});
+  }
+  editProductService(product: Products): Observable<Products>{
+    return <Observable<Products>> this.httpClient.put(`${urlEdit}/${product._id}`,product,{headers: this.headers});
+   }
+  addProductService(product: Products): Observable<Products>{
+    return <Observable<Products>> this.httpClient.post(urlAdd,product,{headers: this.headers});
+  }
+  deleteProductService(_id: number): Observable<Products>{
+    return <Observable<Products>> this.httpClient.delete(`${urlEdit}/${_id}`, {headers: this.headers});
+   }
 }
