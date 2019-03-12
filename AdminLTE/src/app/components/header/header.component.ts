4a8c6e6ae5de884a5f1f6decfecb56fd6c56be23
Đỗ Khoa <momentofm3@gmail.com>
2019-03-12 17:11:18 +0700

    add function checkLogin()

--- AdminLTE/src/app/components/header/header.component.ts
+++ AdminLTE/src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  
+  checkLogin(){
+    if(localStorage.getItem('userToken')){
+      this.router.navigate(['/admin']);
+    }
+  }
 
   ngOnInit() {
   }
